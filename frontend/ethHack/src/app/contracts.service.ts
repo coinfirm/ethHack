34837ce6809d18be1866bae4f950120c35898150
PatrykL <patryk.lucka@gmail.com>
2018-05-19 21:39:14 +0200

    getKey added

--- frontend/ethHack/src/app/contracts.service.ts
+++ frontend/ethHack/src/app/contracts.service.ts
@@ -89,51 +89,15 @@ export class ContractsService {
     return Promise.resolve(this._accounts);
   }
 
-  private async getAccount(): Promise<string> {
-    if (this._account == null) {
-      this._account = await new Promise((resolve, reject) => {
-        this._web3.eth.getAccounts((err, accs) => {
-          if (err != null) {
-            alert('There was an error fetching your accounts.');
-            return;
-          }
-          if (accs.length === 0) {
-            alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.');
-            return;
-          }
-          resolve(accs[0]);
-        });
-      }) as string;
-      this._web3.eth.defaultAccount = this._account;
-    }
-    return Promise.resolve(this._account);
-  }
-
-  public async getBark(): Promise<string> {
-    const account = await this.getAccount();
+  public async getKey(index): Promise<string> {
     return new Promise((resolve, reject) => {
       const _web3 = this._web3;
-      this._tokenContract.bark.call(function (err, result) {
+      this._tokenContract.getKey.call(index, function (err, result) {
         if (err != null) {
           reject(err);
         }
         resolve((result));
       });
     }) as Promise<string>;
-  }
-
-  public async setText(text: string) {
-    this._tokenContract.setText(text, { from: await this.getAccount() }, (err, result) => { });
-  }
-
-  public async getText(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this._tokenContract.text.call((err, result) => {
-        if (err != null) {
-          reject(err);
-        }
-        resolve((result));
-      });
-    });
   }
 }
